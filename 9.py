6a2e8960cfa9eaf3889420d4c6823873199f932d
python <maksdavid2009@gmail.com>
2024-01-21 20:19:22 +0100

    3

--- 9.py
+++ 9.py
@@ -1,20 +1,57 @@
 import sqlite3
-
 connection = sqlite3.connect("db.sl3", 5)
 cur = connection.cursor()
-#CREATE TABLE
-#cur.execute("CREATE TABLE Users (name TEXT)")
 
-#INSERT
-name = input("Add name:   ")
-cur.execute(f"INSERT INTO Users (name) VALUES ('{name}')")
+def create_table():
+    cur.execute("CREATE TABLE IF NOT EXISTS Users (name TEXT);")
 
-#SELECT
-cur.execute("SELECT rowid, name FROM Users; ")
-connection.commit()
+def insert_user():
+    name = input("Введите имя: ")
+    cur.execute(f"INSERT INTO Users (name) VALUES ('{name}');")
 
-res = cur.fetchall()
-print(res)
+def select_users():
+    cur.execute("SELECT rowid, name FROM Users;")
+    res = cur.fetchall()
+    print(res)
+
+def update_user():
+    old_name = input("Введите имя для обновления: ")
+    new_name = input("Введите новое имя: ")
+    cur.execute(f"UPDATE Users SET name='{new_name}' WHERE name='{old_name}';")
+
+def delete_user():
+    name_to_delete = input("Введите имя для удаления: ")
+    cur.execute(f"DELETE FROM Users WHERE name='{name_to_delete}';")
 
 
-connection.close()
+
+
+
+
+successful_login = True
+
+if successful_login:
+    while True:
+        print("1. Добавить пользователя")
+        print("2. Просмотреть пользователей")
+        print("3. Обновить пользователя")
+        print("4. Удалить пользователя")
+        print("5. Выйти")
+
+        choice = input("Введите ваш выбор")
+
+        if choice == '1':
+            insert_user()
+        elif choice == '2':
+            select_users()
+        elif choice == '3':
+            update_user()
+        elif choice == '4':
+            delete_user()
+        elif choice == '5':
+            break
+        else:
+            print("Неверный выбор Пожалуйста введите правильный вариант")
+
+    connection.commit()
+    connection.close()
